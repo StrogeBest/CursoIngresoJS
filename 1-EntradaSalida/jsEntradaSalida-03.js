da446bbf2a70e08516cc72a7acfc12800129fd01
Javier Vilchez <javiervilchez98@gmail.com>
2022-07-04 14:23:44 -0300

    Ejercicio 3 resuelto

--- 1-EntradaSalida/jsEntradaSalida-03.js
+++ 1-EntradaSalida/jsEntradaSalida-03.js
@@ -1,12 +1,18 @@
 /*
+Javier Vilchez
+Div E
+Ejercicio 3 E/S
+*/
+
+/*
 Debemos lograr tomar un dato por 'ID'
-y luego mostrarlo por 'Alert' al presionar el botón  'mostrar'*/
+y luego mostrarlo por 'Alert' al presionar el botón  'mostrar'
+*/
+
 function mostrar()
 {
-	var nombreIngresado;
-	nombreIngresado=txtIdNombre.value;
-	alert("ok");
+	let nombre;
+	nombre = document.getElementById("txtIdNombre").value;
 
+	alert(nombre);
 }
-
-
