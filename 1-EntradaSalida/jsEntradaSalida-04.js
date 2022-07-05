9d240480ed96cf8235e709087db55f6076787b96
Javier Vilchez <javiervilchez98@gmail.com>
2022-07-05 10:52:01 -0300

    Ejercicio 4 resuelto

--- 1-EntradaSalida/jsEntradaSalida-04.js
+++ 1-EntradaSalida/jsEntradaSalida-04.js
@@ -1,9 +1,16 @@
 /* 
+Javier Vilchez
+Div E
+Ejercicio 4 E/S
+
 Debemos lograr tomar un dato por 'PROMPT' 
 y lo muestro por 'ID' al presionar el botón 'mostrar'
 */
+
 function mostrar()
 {
-	alert("ok");
+	let nombre; 
+	nombre = prompt("Ingrese su nombre");
+	
+	document.getElementById("txtIdNombre").value = nombre;
 }
-
