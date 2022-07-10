fcf84c1b44e040e9ca1a6af0fcf6629aa4cbf030
Javier Vilchez <javiervilchez98@gmail.com>
2022-07-10 19:42:00 -0300

    Trabajo practico 3 hecho

--- 8-TPs/03-jsFerretePinturas.js
+++ 8-TPs/03-jsFerretePinturas.js
@@ -1,13 +1,35 @@
-/*3.	Para el departamento de Pinturas:
-A.	Al ingresar una temperatura en Fahrenheit debemos mostrar la temperatura en Centígrados con un mensaje concatenado (ej.: " 32 Fahrenheit son 0 centígrados").
-B.	Al ingresar una temperatura en Centígrados debemos mostrar la temperatura en Fahrenheit (ej.: "0  centígrados son 32 Fahrenheit ").
+/*
+Javier Vilchez
+Div E
+Trabajo Practico Ejercicio 3
+
+3. Para el departamento de Pinturas:
+    A. Al ingresar una temperatura en Fahrenheit debemos mostrar la temperatura en Centígrados con un mensaje concatenado (ej.: " 32 Fahrenheit son 0 centígrados").
+    B. Al ingresar una temperatura en Centígrados debemos mostrar la temperatura en Fahrenheit (ej.: "0  centígrados son 32 Fahrenheit ").
 */
+
 function FahrenheitCentigrados () 
 {
-	
+	let temperatura;
+    let centigrados;
+
+    temperatura = document.getElementById("txtIdTemperatura").value;
+    temperatura = parseInt(temperatura);
+
+    centigrados = (temperatura - 32) * 5/9;
+
+    alert(temperatura + " Fahrenheit son " + centigrados + " centigrados");
 }
 
 function CentigradosFahrenheit () 
 {
-	
+    let temperatura;
+    let fahrenheit;
+    
+    temperatura = document.getElementById("txtIdTemperatura").value;
+    temperatura = parseInt(temperatura);
+    
+    fahrenheit = (temperatura * 9/5) + 32;
+    
+    alert(temperatura + " centigrados son " + fahrenheit + " Fahrenheit");
 }
