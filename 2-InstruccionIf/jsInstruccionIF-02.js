eef0c401ef3ed43b50276c73a77cd7c502472389
Javier Vilchez <javiervilchez98@gmail.com>
2022-07-11 09:28:09 -0300

    Ejercicio 2 del if resuelto

--- 2-InstruccionIf/jsInstruccionIF-02.js
+++ 2-InstruccionIf/jsInstruccionIF-02.js
@@ -1,7 +1,19 @@
+/* 
+Javier Vilchez
+Div E
+Ejercicio 2 IF
+*/
+
 function mostrar()
 {
-	//tomo la edad  
-	alert("ok");
-	
+	// Declaro la variable
+	let edad;
 
+	// Tomo el valor de su id y lo transformo a numero entero
+	edad = parseInt(document.getElementById("txtIdEdad").value);
+
+	// Si es mayor de edad muestro el mensaje
+	if (edad > 18) {
+		alert("Usted es mayor de edad");
+	}
 }//FIN DE LA FUNCIÓN
