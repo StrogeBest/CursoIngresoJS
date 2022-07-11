800fc84ef3c4f616d7279a9bdc755e0a9826ca0a
Javier Vilchez <javiervilchez98@gmail.com>
2022-07-11 09:27:51 -0300

    Ejercicio 1 del if resuelto

--- 2-InstruccionIf/jsInstruccionIF-01.js
+++ 2-InstruccionIf/jsInstruccionIF-01.js
@@ -1,6 +1,19 @@
+/* 
+Javier Vilchez
+Div E
+Ejercicio 1 IF
+*/
+
 function mostrar()
 {
-	//tomo la edad  
-	alert("ok");
+	// Declaro la variable
+	let edad;
 
+	// Tomo el valor de su id y lo transformo a numero entero
+	edad = parseInt(document.getElementById("txtIdEdad").value);
+
+	// Si la edad es 15 muestro el mensaje
+	if (edad == 15) {
+		alert("Niña bonita");
+	}
 }//FIN DE LA FUNCIÓN
