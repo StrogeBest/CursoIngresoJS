82d57a49812883bfecef13c20bccbeed4fb03cd7
Javier Vilchez <javiervilchez98@gmail.com>
2022-07-12 12:59:17 -0300

    Ejercicio 4 del parcial 2018 resuelto

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/cuatro.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/cuatro.js
@@ -1,4 +1,34 @@
+/* 
+Javier Vilchez
+Div E
+Ejercicio 4 Parcial 2018
+*/
+
 function mostrar()
 {
+    let numeroUno;
+    let numeroDos;
+    let resultado;
 
+    numeroUno = prompt("Ingrese el primer numero");
+    numeroDos = prompt("Ingrese el segundo numero");
+
+    if (numeroUno == numeroDos) {
+        alert(numeroUno + numeroDos);
+    }else{
+        numeroUno = parseInt(numeroUno);
+        numeroDos = parseInt(numeroDos);
+        
+        if (numeroUno > numeroDos) {
+            resultado = numeroUno - numeroDos;
+            alert(resultado)   
+        }else{
+            resultado = numeroUno + numeroDos;
+            alert(resultado)   
+            
+            if (resultado > 10) {
+                alert("La suma es " + resultado + " y supero 10")   
+            }
+        }
+    }
 }
