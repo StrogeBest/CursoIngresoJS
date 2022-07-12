935dafd54d8d2512f8283ea3c78ab07fe5493120
Javier Vilchez <javiervilchez98@gmail.com>
2022-07-12 08:42:50 -0300

    Ejercicio 6 del if resuelto

--- 2-InstruccionIf/jsInstruccionIF-06.js
+++ 2-InstruccionIf/jsInstruccionIF-06.js
@@ -1,8 +1,22 @@
+/* 
+Javier Vilchez
+Div E
+Ejercicio 6 IF
+*/
+
 function mostrar()
 {
-	//tomo la edad  
-	alert("ok");
+	let edad;
 
+	edad = parseInt(document.getElementById("txtIdEdad").value);
 
-
+	if (edad >= 18) {
+		alert("Usted es mayor de edad");
+	}else{
+		if (edad >= 13 && edad <= 17) {
+			alert("Usted es adolescente");
+		}else{
+			alert("Usted es niño");
+		}
+	}
 }//FIN DE LA FUNCIÓN
