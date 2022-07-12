aae195e46a04a1dd3bc16f734ea66c2872e5e269
Javier Vilchez <javiervilchez98@gmail.com>
2022-07-12 10:31:19 -0300

    Ejercicio 7 del IF resuelto

--- 2-InstruccionIf/jsInstruccionIF-07.js
+++ 2-InstruccionIf/jsInstruccionIF-07.js
@@ -1,8 +1,18 @@
+/* 
+Javier Vilchez
+Div E
+Ejercicio 7 IF
+*/
+
 function mostrar()
 {
-	//tomo la edad  
-	alert("ok");
-	
+	let edad; 
+	let estadoCivil;
+	 
+	edad = parseInt(document.getElementById("txtIdEdad").value);
+	estadoCivil = document.getElementById("estadoCivil").value;
 
-
+	if (edad < 18 && estadoCivil != "Soltero") {
+		alert("Es muy pequeño para NO ser soltero");
+	}
 }//FIN DE LA FUNCIÓN
