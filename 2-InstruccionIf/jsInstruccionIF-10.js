9f4ed345fec8a7bf5d85da024f14277aea60d8a2
Javier Vilchez <javiervilchez98@gmail.com>
2022-07-12 11:46:22 -0300

    Ejercicio 10 del IF resuelto

--- 2-InstruccionIf/jsInstruccionIF-10.js
+++ 2-InstruccionIf/jsInstruccionIF-10.js
@@ -1,6 +1,23 @@
+/* 
+Javier Vilchez
+Div E
+Ejercicio 10 IF
+*/
+
 function mostrar()
 {
-	//Genero el número RANDOM entre 1 y 10 
-	alert("ok");
+	let nota;
 
+	nota = Math.floor(Math.random() * 10) + 1;
+
+	console.log(nota);
+	if (nota == 9 || nota == 10) {
+		alert("Excelente");
+	}else{
+		if (nota >= 4) {
+			alert("Aprobó");
+		}else{
+			alert("Vamos, la proxima se puede");
+		}
+	}
 }//FIN DE LA FUNCIÓN
