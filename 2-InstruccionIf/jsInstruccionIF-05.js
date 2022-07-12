6c0a2deb108426c4dd4214742c5aa8630d779a0b
Javier Vilchez <javiervilchez98@gmail.com>
2022-07-12 08:42:38 -0300

    Ejercicio 5 del if resuelto

--- 2-InstruccionIf/jsInstruccionIF-05.js
+++ 2-InstruccionIf/jsInstruccionIF-05.js
@@ -1,6 +1,16 @@
+/* 
+Javier Vilchez
+Div E
+Ejercicio 5 IF
+*/
+
 function mostrar()
 {
-	//tomo la edad  
-	alert("ok");
+	let edad;
 
+	edad = parseInt(document.getElementById("txtIdEdad").value);
+
+	if (edad < 13 || edad > 17) {
+		alert("Usted no es adolescente");
+	}
 }//FIN DE LA FUNCIÓN
