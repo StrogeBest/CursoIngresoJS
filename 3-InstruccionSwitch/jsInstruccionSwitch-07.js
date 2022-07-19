c8a907bc7d7062de278adbc5fb8deb5b193db60c
Javier Vilchez <javiervilchez98@gmail.com>
2022-07-19 10:16:58 -0300

    Ejercicio del SWITCH resuelto

--- 3-InstruccionSwitch/jsInstruccionSwitch-07.js
+++ 3-InstruccionSwitch/jsInstruccionSwitch-07.js
@@ -1,6 +1,24 @@
 function mostrar()
 {
-	var destinoIngresado =txtIdDestino.value;
-	alert(destinoIngresado);
+	let destino;
+	let mensaje;
 
+	destino = document.getElementById("txtIdDestino").value;
+
+	switch (destino) {
+		case "Bariloche":
+			mensaje = "Norte"
+			break;
+		case "Cataratas":
+			mensaje = "Norte"
+			break;
+		case "Mar del plata":
+			mensaje = "Entre el Sur y Este"
+			break;
+		default:
+			mensaje = "Sur"
+			break;
+	}
+
+	alert(mensaje);
 }//FIN DE LA FUNCIÓN
