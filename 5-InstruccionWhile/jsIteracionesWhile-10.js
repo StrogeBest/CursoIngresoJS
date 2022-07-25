f4d5ce1b35d467fb35e5c185b0cfeb52be6e99e6
Javier Vilchez <javiervilchez98@gmail.com>
2022-07-25 12:17:59 -0300

    Ejercicio 10 del WHILE resuelto

--- 5-InstruccionWhile/jsIteracionesWhile-10.js
+++ 5-InstruccionWhile/jsIteracionesWhile-10.js
@@ -1,4 +1,8 @@
 /*
+Javier Vilchez
+Div E
+Ejercicio 10 WHILE
+
 Al presionar el botón pedir números hasta que el usuario quiera, mostrar:
 	1-Suma de los negativos.
 	2-Suma de los positivos.
@@ -13,20 +17,69 @@ Al presionar el botón pedir números hasta que el usuario quiera, mostrar:
 
 function mostrar()
 {
-	//declarar contadores y variables 
+	// Declarar contadores y variables 
 	let respuesta;
 	let numeroIngresado;
-	let sumaNegativos=0;
+	let sumaPositivos = 0;
+	let sumaNegativos = 0;
+	let cantidadPositivos = 0;
+	let cantidadNegativos = 0;
+	let cantidadCeros = 0;
+	let cantidadNumeroPar = 0;
+	let promedioPositivos;
+	let promedioNegativos;
+	let diferenciaPositivoNegativo;
 
-	respuesta="si";
+	respuesta = "si";
 
-	while(respuesta=="si")
+	while(respuesta == "si")
 	{
-		if (numeroIngresado < 0) {
-			sumaNegativos = sumaNegativos + numeroIngresado;
+		// Pido el numero
+		numeroIngresado = parseInt(prompt("Ingrese un numero"));
+
+		// PUNTO 5
+		if (numeroIngresado == 0) {
+			cantidadCeros++;
+		}else{
+			// PUNTO 2
+			if (numeroIngresado > 0) {
+				sumaPositivos = sumaPositivos + numeroIngresado;
+
+				// PUNTO 3
+				cantidadPositivos++;
+			}else{
+				// PUNTO 1
+				sumaNegativos = sumaNegativos + numeroIngresado;
+
+				// PUNTO 4
+				cantidadNegativos++;
+			}
 		}
-		respuesta=prompt("desea continuar?");
+
+		// PUNTO 6
+		if (numeroIngresado % 2 == 0) {
+			cantidadNumeroPar++;
+		}
+
+		respuesta = prompt("¿Desea continuar? si/no");
 	}//fin del while
 
-	document.write("la suma de negativos es :" + sumaNegativos);
+	// PUNTO 7
+	promedioPositivos = sumaPositivos / cantidadPositivos;
+	// PUNTO 8
+	promedioNegativos = sumaNegativos / cantidadNegativos;
+	// PUNTO 9
+	diferenciaPositivoNegativo = sumaPositivos - sumaNegativos;
+
+	document.write(
+		"1- La suma de numeros positivos es: " + sumaPositivos + "<br>" +
+		"2- La suma de numeros negativos es: " + sumaNegativos + "<br>" +
+		"3- La cantidad de numeros positivos es: " + cantidadPositivos + "<br>" +
+		"4- La cantidad de numeros negativos es: " + cantidadNegativos + "<br>" +
+		"5- La cantidad de ceros es: " + cantidadCeros + "<br>" +
+		"6- La cantidad de numeros pares es: " + cantidadNumeroPar + "<br>" +
+		"7- El promedio de los numeros positivos es: " + promedioPositivos + "<br>" +
+		"8- El promedio de los numeros negativos es: " + promedioNegativos + "<br>" +
+		"9- La diferencia entre numeros positivos y negativos es: " + diferenciaPositivoNegativo
+	);
 }//FIN DE LA FUNCIÓN
