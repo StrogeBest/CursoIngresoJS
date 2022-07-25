547193f7e5977db116ade22167905a7860282f6d
Javier Vilchez <javiervilchez98@gmail.com>
2022-07-25 10:12:15 -0300

    Ejercicio 8 WHILE resuelto

--- 5-InstruccionWhile/jsIteracionesWhile-08.js
+++ 5-InstruccionWhile/jsIteracionesWhile-08.js
@@ -1,19 +1,35 @@
 /*
-Al presionar el botón pedir  números  hasta que el usuario quiera,
-sumar los que son positivos y multiplicar los negativos.*/
+Javier Vilchez
+Div E
+Ejercicio 8 WHILE
+
+Al presionar el botón pedir  números  hasta que el usuario quiera, sumar los que son positivos y multiplicar los negativos.
+*/
+
 function mostrar()
 {
-	var contador;
-	var respuesta;
-	var sumaPositivos;
-	var multiplicacionNegativos;
-	contador=0;
-	sumaPositivos=0;
-	multiplicacionNegativos=1;
-	respuesta='si';
+	let respuesta;
+	let sumaPositivos;
+	let multiplicacionNegativos;
+	let numeroIngresado;
 
+	contador = 0;
+	sumaPositivos = 0;
+	multiplicacionNegativos = 1;
+	respuesta = "si";
 
-	txtIdSuma.value=sumaPositivos;
-	txtIdProducto.value=multiplicacionNegativos;
+	while (respuesta == "si") {
+		numeroIngresado = parseInt(prompt("Ingrese un numero"));
+		
+		if (numeroIngresado >= 0) {
+			sumaPositivos = sumaPositivos + numeroIngresado;
+		}else{
+			multiplicacionNegativos = multiplicacionNegativos * numeroIngresado;
+		}
 
+		respuesta = prompt("¿Desea continuar? si/no");
+	}
+
+	document.getElementById("txtIdSuma").value = sumaPositivos;
+	document.getElementById("txtIdProducto").value = multiplicacionNegativos;
 }//FIN DE LA FUNCIÓN
