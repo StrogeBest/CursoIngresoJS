f7f31292c38414f5dbd5d15b3000330d20f9cdf8
Javier Vilchez <javiervilchez98@gmail.com>
2022-07-26 08:58:13 -0300

    TP 12 - Mejorando el codigo

--- 8-TPs/12-jsRisingBTL.js
+++ 8-TPs/12-jsRisingBTL.js
@@ -11,10 +11,11 @@ RISING BTL. Empresa dedicada a la toma de datos para realizar estadísticas y ce
 		D.	Sueldo bruto, no menor a 8000.
 		E.	Número de legajo, numérico de 4 cifras, sin ceros a la izquierda.
 		F.	Nacionalidad, “A” para argentinos, “E” para extranjeros, “N” para nacionalizados.
- */
+*/
+
 function ComenzarIngreso () 
 {
- 	//definicion de variables
+ 	// Definicion de variables
  	let edadIngresada;
  	let sexoIngresado;
  	let estadoCivilIngresado;
@@ -23,54 +24,45 @@ function ComenzarIngreso ()
 	let nacionalidadIngresada;
 
 	// PUNTO A
-	edadIngresada = prompt("Ingrese su edad");
-	edadIngresada = parseInt(edadIngresada);
-	while (edadIngresada < 18 || edadIngresada > 90) {
-		edadIngresada = prompt("ERROR, Ingrese su edad entre 18 y 90");
+	edadIngresada = parseInt(prompt("Ingrese su edad entre 18 y 90"));
+	while (isNaN(edadIngresada) || edadIngresada < 18 || edadIngresada > 90) {
+		edadIngresada = parseInt(prompt("ERROR. Ingrese su edad entre 18 y 90"));
 	}
-	// Muestro la edad ingresada
-	document.getElementById("txtIdEdad").value = edadIngresada;
 	
 	// PUNTO B
 	sexoIngresado = prompt("Ingrese su sexo m(masculino) o f(femenino)");
 	while (sexoIngresado != "m" && sexoIngresado != "f") {
-		sexoIngresado = prompt("ERROR, Ingrese su sexo m(masculino) o f(femenino)");
+		sexoIngresado = prompt("ERROR. Ingrese su sexo m(masculino) o f(femenino)");
 	}
-	// Muestro el sexo
-	document.getElementById("txtIdSexo").value = sexoIngresado;
 	
 	// PUNTO C
-	estadoCivilIngresado = prompt("Ingrese su estado civil: 1-para soltero, 2-para casado, 3-para divorciado y 4-para viudo");
-	estadoCivilIngresado = parseInt(estadoCivilIngresado);
+	estadoCivilIngresado = parseInt(prompt("Ingrese su estado civil: 1-para soltero, 2-para casado, 3-para divorciado y 4-para viudo"));
 	while (estadoCivilIngresado != 1 && estadoCivilIngresado != 2 && estadoCivilIngresado != 3 && estadoCivilIngresado != 4) {
-		estadoCivilIngresado = prompt("ERROR, Ingrese su estado civil: 1-para soltero, 2-para casado, 3-para divorciado y 4-para viudo");
+		estadoCivilIngresado = parseInt(prompt("ERROR. Ingrese su estado civil: 1-para soltero, 2-para casado, 3-para divorciado y 4-para viudo"));
 	}
-	// Muestro el estado civil
-	document.getElementById("txtIdEstadoCivil").value = estadoCivilIngresado;
 	
 	// PUNTO D
-	sueldoIngresado = prompt("Ingrese su sueldo");
-	sueldoIngresado = parseInt(sueldoIngresado);
-	while (sueldoIngresado < 8000) {
-		sueldoIngresado = prompt("ERROR. El sueldo debe ser mayor a 8000, ingrese su sueldo");
+	sueldoIngresado = parseInt(prompt("Ingrese su sueldo (mayor a 8000)"));
+	while (isNaN(sueldoIngresado) || sueldoIngresado < 8000) {
+		sueldoIngresado = parseInt(prompt("ERROR. Ingrese su sueldo (mayor a 8000)"));
 	}
-	// Muestro el sueldo
-	document.getElementById("txtIdSueldo").value = sueldoIngresado;
 	
 	// PUNTO E
-	legajoIngresado = prompt("Ingrese el numero de legajo (4 cifras)");
-	legajoIngresado = parseInt(legajoIngresado);
-	while (legajoIngresado < 1000) {
-		legajoIngresado = prompt("ERROR, Ingrese el numero de legajo (4 cifras)");
+	legajoIngresado = parseInt(prompt("Ingrese el numero de legajo (4 cifras)"));
+	while (legajoIngresado < 1000 || legajoIngresado > 9999) {
+		legajoIngresado = parseInt(prompt("ERROR. Ingrese el numero de legajo (4 cifras)"));
 	}
-	// Muestro el legajo
-	document.getElementById("txtIdLegajo").value = legajoIngresado;
 	
 	// PUNTO F
 	nacionalidadIngresada = prompt("Ingrese su nacionalidad: A para argentinos, E para extranjeros, N para nacionalizados");
 	while (nacionalidadIngresada != "A" && nacionalidadIngresada != "E" && nacionalidadIngresada != "N") {
 		nacionalidadIngresada = prompt("ERROR. Ingrese su nacionalidad: A para argentinos, E para extranjeros, N para nacionalizados");
 	}
-	// Muestro la nacionalidad
+
+	document.getElementById("txtIdEdad").value = edadIngresada;
+	document.getElementById("txtIdSexo").value = sexoIngresado;
+	document.getElementById("txtIdEstadoCivil").value = estadoCivilIngresado;
+	document.getElementById("txtIdSueldo").value = sueldoIngresado;
+	document.getElementById("txtIdLegajo").value = legajoIngresado;
 	document.getElementById("txtIdNacionalidad").value = nacionalidadIngresada;
 }
